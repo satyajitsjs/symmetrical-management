8ef25d55dc841d573d429fcab561729fa9d75b32
Satyajit2024 <satyajit.bariflo@outlook.com>
2024-05-28 19:30:06 +0530

    frontend app

--- gate_management_system/management/serializers.py
+++ gate_management_system/management/serializers.py
@@ -1,10 +1,26 @@
 from rest_framework import serializers
 from .models import Gate, GuardAssignment
+from datetime import datetime
 
 class GateSerializer(serializers.ModelSerializer):
+    start_time = serializers.CharField()
+    end_time = serializers.CharField()
+
     class Meta:
         model = Gate
         fields = '__all__'
+
+    def validate_start_time(self, value):
+        return self.convert_time_format(value)
+
+    def validate_end_time(self, value):
+        return self.convert_time_format(value)
+
+    def convert_time_format(self, value):
+        try:
+            return datetime.strptime(value, '%I.%M %p').time()
+        except ValueError:
+            raise serializers.ValidationError("Time has wrong format. Use one of these formats instead: hh:mm[:ss[.uuuuuu]].")
 
 class GuardAssignmentSerializer(serializers.ModelSerializer):
     class Meta:
